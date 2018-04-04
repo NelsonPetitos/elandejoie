e9b056a5f4a542ef7a9f966ba98169a1cfb9fa4c
Nde Nelson <ndenelson@Ndes-MacBook-Pro.local>
2018-04-04 16:42:10 +0100

    Before formatage

--- server.js
+++ server.js
@@ -16,35 +16,41 @@ app.use('/assets', express.static(path.join(__dirname, 'public')));
 
 // Catch all other routes and return the index file
 app.get('/', (request, response)  => {
+    console.log('Route : home');
     response.render('pages/index', {active: 1});
     // res.sendFile(path.join(__dirname, 'pages/index.html'));
 });
 
 
 app.get('/about', (request, response)  => {
+    console.log('Route : about');
     response.render('pages/about', {active: 2});
     // res.sendFile(path.join(__dirname, 'pages/index.html'));
 });
 
 
 app.get('/services', (request, response)  => {
+    console.log('Route : services');
     response.render('pages/services', {active: 3});
     // res.sendFile(path.join(__dirname, 'pages/index.html'));
 });
 
 
 app.get('/products', (request, response)  => {
+    console.log('Route : products');
     response.render('pages/products', {active: 4});
     // res.sendFile(path.join(__dirname, 'pages/index.html'));
 });
 
 app.get('/blog', (request, response)  => {
+    console.log('Route : blog');
     response.render('pages/blog', {active: 5});
     // res.sendFile(path.join(__dirname, 'pages/index.html'));
 });
 
 
-app.get('/contact', (request, response)  => {
+app.get('/contacts', (request, response)  => {
+    console.log('Route : contacts');
     response.render('pages/contact', {active: 6});
     // res.sendFile(path.join(__dirname, 'pages/index.html'));
 });
@@ -52,6 +58,7 @@ app.get('/contact', (request, response)  => {
 
 // Catch all other routes and return the index file
 app.get('*', (request, response)  => {
+    console.log('Route : default');
     response.render('pages/index', {active: 1});
     // res.sendFile(path.join(__dirname, 'src/index.html'));
 });
